badc162d4fa4e9a3811f3f1d44267bb153051d84
Cameron Chafkar <ccdecisions@gmail.com>
2018-11-29 22:00:52 -0800

    add messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -5,9 +5,12 @@ class MessageList extends Component {
     super(props);
 
     this.state = {
-      messages: []
+      messages: [],
+      newMessage: ''
   }
     this.messagesRef = this.props.firebase.database().ref("messages");
+    this.handleChange = this.handleChange.bind(this);
+    this.createMessage = this.createMessage.bind(this);
 
   }
 
@@ -19,17 +22,39 @@ class MessageList extends Component {
       });
   }
 
+  handleChange(event){
+    this.setState({ newMessage: event.target.value });
+  }
+
+  createMessage(event){
+    event.preventDefault();
+    this.messagesRef.push({
+      content: this.state.newMessage,
+      roomID: this.props.currentRoom.key,
+      sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
+      username: this.props.user ? this.props.user.displayName: ''
+    });
+  }
 
   render(){
     return(
       <section className="message-content">
         <h1>{this.props.currentRoom.name}</h1>
-        <div>{this.state.messages.filter(message => message.roomID === this.props.currentRoom.key).map(message =>
-          <div className="room-content" key={message.key}>
-            {message.content}
+
+      <div>{this.state.messages.filter(message => message.roomID === this.props.currentRoom.key).map(message =>
+          <div className="message-list" key={message.key}>
+            <div>{message.content}</div>
+            <div>{message.username}</div>
+            <div>{message.sentAt}</div>
           </div>
-        )}
-        </div>
+          )}
+         </div>
+         <form className="new-messages" onSubmit={this.createMessage}>
+          <label className="message-text">
+            <input type="textbox" onChange={this.handleChange} className="message-content" placeholder="say something" />
+          </label>
+          <input className="btn btn-secondary" type="submit" value="submit" />
+         </form>
       </section>
     )
   }
